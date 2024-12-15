f407daa9aac421f069ae6a72e3c7f89c374ce2cb
marcobuess <42165023+MarcoBuess@users.noreply.github.com>
2024-12-15 19:20:49 +0100

    feat: transparent pink theme

--- lua/lualine/themes/ayu.lua
+++ lua/lualine/themes/ayu.lua
@@ -1,35 +1,36 @@
 local colors = require('ayu.colors')
+local neon_pink = '#ff4bff'
 
 local ayu = {
   visual = {
     a = { fg = colors.bg, bg = colors.accent, gui = 'bold' },
-    b = { fg = colors.accent, bg = colors.panel_border },
+    b = { fg = colors.accent, bg = nil },
   },
   replace = {
     a = { fg = colors.bg, bg = colors.markup, gui = 'bold' },
-    b = { fg = colors.markup, bg = colors.panel_border },
+    b = { fg = colors.markup, bg = nil },
   },
   inactive = {
     a = { fg = colors.fg, bg = colors.panel_border, gui = 'bold' },
-    b = { fg = colors.fg, bg = colors.panel_border },
-    c = { fg = colors.fg, bg = colors.panel_border },
+    b = { fg = colors.bg, bg = nil },
+    c = { fg = colors.bg, bg = nil },
   },
   normal = {
-    a = { fg = colors.bg, bg = colors.entity, gui = 'bold' },
-    b = { fg = colors.entity, bg = colors.panel_border },
-    c = { fg = colors.fg, bg = colors.panel_border },
+    a = { fg = colors.bg, bg = neon_pink, gui = 'bold' },
+    b = { fg = neon_pink, bg = nil },
+    c = { fg = colors.bg, bg = nil },
   },
   insert = {
     a = { fg = colors.bg, bg = colors.string, gui = 'bold' },
-    b = { fg = colors.string, bg = colors.panel_border },
+    b = { fg = colors.string, bg = nil },
   },
   command = {
-    a = { fg = colors.bg, bg = colors.constant, gui = 'bold' },
-    b = { fg = colors.constant, bg = colors.panel_border },
+    a = { fg = colors.bg, bg = colors.entity, gui = 'bold' },
+    b = { fg = colors.entity, bg = nil },
   },
   terminal = {
     a = { fg = colors.bg, bg = colors.string, gui = 'bold' },
-    b = { fg = colors.string, bg = colors.panel_border },
+    b = { fg = colors.string, bg = nil },
   },
 }
 
